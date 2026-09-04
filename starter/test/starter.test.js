120db887ee9d5acd2eb24a26eae7022f915d5f89
Eric Artzt <artzt@juniperswordplay.com>
2026-09-04 14:04:23 -0700

    Enable watch polling in Vite and add start script

--- starter/test/starter.test.js
+++ starter/test/starter.test.js
@@ -1,13 +1,17 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, beforeEach } from 'vitest'
 import { renderApp } from '../src/app.js'
 
 describe('starter kit sanity check', () => {
+  beforeEach(() => {
+    console.log('invoking renderApp()')
+    renderApp()
+  })
+
   it('can query by selector', () => {
     expect(document.querySelector('#app')).not.toBeNull()
   })
 
   it('renders starter message with renderApp()', () => {
-    renderApp()
     const app = document.querySelector('#app')
     expect(app.textContent).toBe('Hello, JavaScript!')
   })
--- starter/vite.config.js
+++ starter/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: './',
+  server: {
+    watch: {
+      usePolling: true,
+    },
+  },
   test: {
     environment: 'jsdom',
     setupFiles: ['./test/setup.js'],
